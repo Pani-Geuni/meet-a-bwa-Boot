f56b0dad6bf6cef01e20d4912bd71b102e530520
PanGeun <lifebook0809@gmail.com>
2022-12-06 11:10:32 +0900

    [refactor] Vote select SQL 수정

--- src/main/java/com/mab/meet/repository/MeetVoteRepository.java
+++ src/main/java/com/mab/meet/repository/MeetVoteRepository.java
@@ -16,7 +16,7 @@ public interface MeetVoteRepository extends JpaRepository<MeetVoteVO, Object> {
 
 	
 	@Query(nativeQuery = true,
-			value="select vote_no, vote_title, user_no, meet_no "
+			value="select * "
 					+ "from vote where meet_no=?1 order by vote_no desc")
 	public List<MeetVoteVO> select_all_vote_meet(String meet_no);
 }
